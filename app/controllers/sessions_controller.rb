de36a2ef0f9a843224714551ce65dd45e44a3672
astarael7 <astarael7@gmail.com>
2013-02-12 17:27:25 -0500

    Added Flash tag for invalid input

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,8 @@ class SessionsController < ApplicationController
 		if user && user.authenticate(params[:session][:password])
 			# Sign in the user and redirect to the user's page.
 		else
-			# Create an error message and re-render the signin form.
+			flash.now[:error] = 'Invalid email/password combination'
+			render 'new'
 		end
 	end
 
