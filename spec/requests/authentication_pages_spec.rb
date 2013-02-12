5bc8c15fd8ab9240456b8716ee516f4d0f436d4c
astarael7 <astarael7@gmail.com>
2013-02-12 17:25:51 -0500

    Filled out signin page and added tests for invalid input

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -12,5 +12,19 @@ describe "Authentication" do
 			it { should have_selector('title', text: 'Sign In') }
 			it { should have_selector('div.alert.alert-error', text: 'Invalid') }
 		end
+
+		describe "with valid information" do
+			let (:user) { FactoryGirl.create(:user) }
+			before do
+				fill_in "Email",	with: user.email.upcase
+				fill_in "Password",	with: user.password
+				click_button "Sign In"
+			end
+
+			it { should have_selector('title', text: user.name) }
+			it { should have_link('Profile', href: user_path(user)) }
+			it { should have_link('Sign Out', href: signout_path) }
+			it { should_not have_link('Sign In', href: signin_path) }
+		end
 	end
 end
