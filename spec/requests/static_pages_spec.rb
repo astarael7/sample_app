d9610294c2e5a2b59c72b71ba2fa444f3a313664
Jason Chance <astarael7@gmail.com>
2013-02-17 14:06:43 -0500

    Reidentified application as my own work

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,7 +7,7 @@ describe "Static pages" do
 describe "Home page" do
     before { visit root_path }
 
-    it { should have_selector('h1', text: 'Sample App') }
+    it { should have_selector('h1', text: 'Bloggle') }
     it { should have_selector('title', text: '') }
     it { should_not have_selector 'title', text: '| Home' }
   end
@@ -16,16 +16,9 @@ describe "Home page" do
     it { should have_title(title) }
   end
 
-  describe "Help page" do
-    before { visit help_path }
-    let(:title)     { 'Help' }
-
-    it_should_behave_like "all static pages"
-  end
-
   describe "About page" do
     before { visit about_path }
-    let(:title)     { 'About Us' }
+    let(:title)     { 'About This' }
 
     it_should_behave_like "all static pages"
   end
@@ -40,15 +33,13 @@ describe "Home page" do
   it "should have the right links on the layout" do
     visit root_path
     click_link "About"
-    page.should have_selector 'title', text: full_title('About Us')
-    click_link "Help"
-    page.should have_selector 'title', text: full_title('Help')
+    page.should have_selector 'title', text: full_title('About This')
     click_link "Contact"
     page.should have_selector 'title', text: full_title('Contact')
     click_link "Home"
     click_link "Sign up now!"
     page.should have_selector 'title', text: full_title('Sign up')
-    click_link "sample app"
+    click_link "Bloggle"
     page.should have_selector 'title', text: full_title('')
   end
 end
