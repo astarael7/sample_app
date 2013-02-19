10b593dcf479a95d596293d905f48a534276161d
Jason Chance <astarael7@gmail.com>
2013-02-19 17:25:25 -0500

    Posts now link to primitive display

--- spec/requests/post_pages_spec.rb
+++ spec/requests/post_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "Posts" do
 
+	subject { page }
+
 	let(:user) { FactoryGirl.create(:user) }
 	let!(:p1) { FactoryGirl.create(:post, user: user, title: "Foo", content: "bar") }
 
@@ -9,7 +11,8 @@ describe "Posts" do
 		sign_in user
 		visit user_post_path(user, p1)
 	end
-
-	it { should have_title(p1.title) }
 	
+	it { should have_title(p1.title) }
+	it { should have_content(p1.content) }
+	it { should have_content(p1.user.name) }
 end
