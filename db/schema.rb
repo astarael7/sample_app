00401325661c44ea9d31949ca251900c5ec642ac
Jason Chance <astarael7@gmail.com>
2013-03-05 10:44:41 -0500

    Readying master

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130219172310) do
+ActiveRecord::Schema.define(:version => 20130305152403) do
 
   create_table "posts", :force => true do |t|
     t.string   "title"
